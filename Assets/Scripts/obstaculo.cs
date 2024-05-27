42b337f64969dd2c28d382713b21081961759fb5
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-05-27 11:26:09 -0300

    obstaculo

--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -7,6 +7,18 @@ public class obstaculo : MonoBehaviour
     [SerializeField]
     private float velocidade = 1.5f;
 
+    [SerializeField]
+    private float variacaoposicaoY;
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY, variacaoposicaoY));
+    }
+
     // Update is called once per frame
     void Update()
     {
