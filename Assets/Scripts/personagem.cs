3ce0a4979b9c81e5a6ea96730cd7b14b44463155
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-06-19 13:38:23 -0300

    ac

--- Assets/Scripts/personagem.cs
+++ Assets/Scripts/personagem.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class personagem : MonoBehaviour
 {
+    //cria a variavel fisica do tipo Rigidbody
     Rigidbody2D fisica;
-    private float velocidade = 3;
 
+    //método Awake é chamado toda vez que o objeto é criado
     private void Awake()
     {
-        this.fisica = GetComponent<Rigidbody2D>();   
+        //adiciona à variavel física o componente RigidBody daquele objeto ativo no momento
+        this.fisica = this.GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    //métodos private somente nossa classe pode acessar
+    private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Clicou");
+            //chama o método impulsionar
+            //o this serve para indicar que é o objeto ativo naquele momento que recerá ação
             this.Impulsionar();
         }
     }
-
-    void Impulsionar()
+    //método que impulsiona nosso pássaro
+    private void Impulsionar()
     {
-        this.fisica.AddForce(Vector2.up * velocidade, ForceMode2D.Impulse);
+        //zera a velocidade antes de dar o impulso
+        this.fisica.velocity = Vector2.zero;
+        //adiciona a variável física e o método AddForce, adicionando uma força para cima do tipo impulso
+        this.fisica.AddForce(Vector2.up * 3, ForceMode2D.Impulse);
+    }
+    private void OnCollisionEnter2D(Collision2D colision)
+    {
+        this.fisica.simulated = false;
     }
 }
